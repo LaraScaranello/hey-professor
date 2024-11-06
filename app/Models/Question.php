4b31baa207fe64c89bdc17d65a07158b4de6dd1f
Lara Scaranello <ti@grindelia.com.br>
2024-11-06 13:53:57 -0300

    HEY-14: adding like button to our interface

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,11 +3,38 @@
 namespace App\Models;
 
 use Database\Factories\QuestionFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
     /** @use HasFactory<QuestionFactory> */
     use HasFactory;
+
+    /**
+     * @return HasMany<Vote, User>
+     */
+    public function votes(): HasMany
+    {
+        /** @var HasMany<Vote, User> */
+        return $this->hasMany(Vote::class);
+    }
+
+    /**
+     * @return Attribute<int, null>
+     */
+    public function likes(): Attribute
+    {
+        return new Attribute(get: fn (): int => $this->votes()->sum('like'));
+    }
+
+    /**
+     * @return Attribute<int, null>
+     */
+    public function unlikes(): Attribute
+    {
+        return new Attribute(get: fn (): int => $this->votes()->sum('unlike'));
+    }
 }
