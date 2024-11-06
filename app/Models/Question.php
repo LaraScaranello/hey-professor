8bb1cf2023f2998f0a30892b37399f438416e0b0
Lara Scaranello <ti@grindelia.com.br>
2024-11-06 14:25:02 -0300

    HEY-14: handling N+1 issue

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Database\Factories\QuestionFactory;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -20,21 +19,5 @@ class Question extends Model
     {
         /** @var HasMany<Vote, User> */
         return $this->hasMany(Vote::class);
-    }
-
-    /**
-     * @return Attribute<int, null>
-     */
-    public function likes(): Attribute
-    {
-        return new Attribute(get: fn (): int => $this->votes()->sum('like'));
-    }
-
-    /**
-     * @return Attribute<int, null>
-     */
-    public function unlikes(): Attribute
-    {
-        return new Attribute(get: fn (): int => $this->votes()->sum('unlike'));
     }
 }
