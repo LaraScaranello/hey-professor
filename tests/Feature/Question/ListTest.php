cac45aca5436f57e7cb48e4b0ee4a86302581025
Lara Scaranello <ti@grindelia.com.br>
2024-11-11 14:29:19 -0300

    hey-20: ordering base on likes/unlikes

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -38,3 +38,30 @@ it('should paginate the result', function () {
             fn ($value) => $value instanceof LengthAwarePaginator
         );
 });
+
+it('should order by like and unlike, most liked questions should be at the top, most unlike questions should be in the bottom', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+
+    $mostLikedQuestion = Question::find(3);
+    $user->like($mostLikedQuestion);
+
+    $mostUnlikeQuestion = Question::find(1);
+    $secondUser->unlike($mostUnlikeQuestion);
+
+    actingAs($user);
+
+    get(route('dashboard'))
+        ->assertViewHas(
+            'questions',
+            function ($questions) {
+                expect($questions)
+                    ->first()->id->toBe(3)
+                    ->and($questions)
+                    ->last()->id->toBe(1);
+
+                return true;
+            }
+        );
+});
