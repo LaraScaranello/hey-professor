0824fcda73a11761209694e6b0bed0e93e8b03c4
Lara Scaranello <ti@grindelia.com.br>
2024-11-11 10:25:16 -0300

    hey-18: should make sure that only question with status DRAFT and only if the question was created by the logged user can be updated

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -55,6 +55,8 @@ class QuestionController extends Controller
 
     public function update(Question $question): RedirectResponse
     {
+        $this->authorize('update', $question);
+
         $question->question = request()->question;
         $question->save();
 
