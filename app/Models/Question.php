7fba88bc760f84a987425363721f4c1896118fa2
Lara Scaranello <ti@grindelia.com.br>
2024-11-11 16:21:03 -0300

    hey-22: it should prune a question archived mre than a month

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Database\Factories\QuestionFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
-use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\{Builder, Model, Prunable, SoftDeletes};
 
 class Question extends Model
 {
     /** @use HasFactory<QuestionFactory> */
     use HasFactory;
     use SoftDeletes;
+    use Prunable;
 
     protected $casts = [
         'draft' => 'boolean',
@@ -33,5 +34,13 @@ class Question extends Model
     {
         /** @var BelongsTo<User, Question> */
         return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * @return Builder<Question>
+     */
+    public function prunable(): Builder
+    {
+        return static::where('deleted_at', '<=', now()->subMonth());
     }
 }
